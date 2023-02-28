13b3d574feaf827ac6d4389408aa16f5e99df42f
l2csi2022 <iscgomal2csi@gmail.com>
2023-02-28 21:27:04 +0200

    Ajout des labels

---
+++ Essai_L2_CSI/Essai_L2_CSI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Essai_L2_CSI
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
